1a9f6693dc0487507af6cb07c5944b46f22f9012
maxwellmuchiri <muchirimaxwel@gmail.com>
2023-09-10 13:08:22 +0300

    Grouping Products

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,100 @@
 
+// import React, { useState } from "react";
+// import { Link } from "react-router-dom";
+// import { FaShoppingCart } from "react-icons/fa"; // Import FaShoppingCart icon
 
-import React from "react";
+// const Navbar = ({ loggedIn, logOut, cartItems }) => {
+//   // State to manage the visibility of the dropdown
+//   const [isDropdownVisible, setDropdownVisible] = useState(false);
+
+//   // Function to toggle the dropdown's visibility
+//   const toggleDropdown = () => {
+//     setDropdownVisible(!isDropdownVisible);
+//   };
+
+//   // Function to hide the dropdown when a link is clicked
+//   const hideDropdown = () => {
+//     setDropdownVisible(false);
+//   };
+
+//   return (
+//     <nav className="navbar">
+//       <div className="logo">
+//         <img
+//           src="https://scontent.fnbo1-1.fna.fbcdn.net/v/t39.30808-6/368009851_613450754242703_2035892794540794207_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_ohc=2McAvRjlrH8AX_Ocqhw&_nc_ht=scontent.fnbo1-1.fna&oh=00_AfCKveOjUoD9CyuXP7utmbBwTWuI6eYIaLS8zwp9yNQcMg&oe=64F300C9"
+//           alt="Sign Logo"
+//         />
+//         <Link to="/">ELIM SIGNS</Link> {/* Link to the landing page */}
+//       </div>
+
+//       <div className="menu">
+//         <div className="menu-item">
+//           <Link to="/">HOME</Link>
+//         </div>
+//         <div className="menu-item">
+//           <span
+//             onClick={toggleDropdown}
+//             onMouseEnter={toggleDropdown}
+//             onMouseLeave={toggleDropdown}
+//           >
+//             PRODUCTS & SERVICES
+//             {/* Dropdown content */}
+//             {isDropdownVisible && (
+//               <div className="dropdown-content" onMouseLeave={hideDropdown}>
+//                 <Link to="/products/3d-signs">3D Signs</Link>
+//                 <Link to="/products/2d-signs">2D Signs</Link>
+//                 <Link to="/products/road-signs">Road Signs</Link>
+//                 <Link to="/products/cnc-cuts">CNC Cuts</Link>
+//                 <Link to="/products/laser-cuts">Laser Cuts</Link>
+//                 <Link to="/products/branding">Branding</Link>
+//                 <Link to="/products/signages">Signages</Link>
+//                 <Link to="/products/paylons">Paylons</Link>
+//                 <Link to="/products/engravings">Engravings</Link>
+//               </div>
+//             )}
+//           </span>
+//         </div>
+//         <div className="menu-item">
+//           <Link to="/about-us">About Us</Link> {/* Link to the About Us page */}
+//         </div>
+//         <div className="menu-item">
+//           <Link to="/contacts">Talk to Us</Link> {/* Link to the Contacts page */}
+//         </div>
+//         <div className="menu-item">
+//           <Link to="/cart">
+//             <FaShoppingCart /> {/* Cart Icon */}
+//           </Link>
+//         </div>
+//       </div>
+
+//       {/* User-related links */}
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
+
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaShoppingCart } from "react-icons/fa"; // Import FaShoppingCart icon
 
 const Navbar = ({ loggedIn, logOut, cartItems }) => {
+  // State to manage the visibility of the dropdown
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
+
+  // Function to toggle the dropdown's visibility
+  const toggleDropdown = () => {
+    setDropdownVisible(!isDropdownVisible);
+  };
+
+  // Function to hide the dropdown when a link is clicked
+  const hideDropdown = () => {
+    setDropdownVisible(false);
+  };
+
   return (
-    <nav>
-      <div>
+    <nav className="navbar">
+      <div className="logo">
         <img
           src="https://scontent.fnbo1-1.fna.fbcdn.net/v/t39.30808-6/368009851_613450754242703_2035892794540794207_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_ohc=2McAvRjlrH8AX_Ocqhw&_nc_ht=scontent.fnbo1-1.fna&oh=00_AfCKveOjUoD9CyuXP7utmbBwTWuI6eYIaLS8zwp9yNQcMg&oe=64F300C9"
           alt="Sign Logo"
@@ -15,23 +102,45 @@ const Navbar = ({ loggedIn, logOut, cartItems }) => {
         <Link to="/">ELIM SIGNS</Link> {/* Link to the landing page */}
       </div>
 
-    
-      <div>
-        <Link to="/">HOME</Link>
-       </div> 
-       <div>
-        <Link to="/products">PRODUCTS & SERVICES</Link>
-        {/* Add the "Products" tab with a link to "/products" */}
-      </div>
-      <div>
-      <Link to="/about-us">About Us</Link> {/* Link to the About Us page */}
-        <Link to="/contacts">Talk to Us</Link> {/* Link to the Contacts page */}
-      </div>
-
-      <div>
-        <Link to="/cart">
-          <FaShoppingCart /> {/* Cart Icon */}
-        </Link>
+      <div className="menu">
+        <div className="menu-item">
+          <Link to="/">HOME</Link>
+        </div>
+        <div className="menu-item">
+          <span
+            onClick={toggleDropdown}
+            onMouseEnter={toggleDropdown}
+            onMouseLeave={toggleDropdown}
+          >
+            PRODUCTS & SERVICES
+            {/* Dropdown content */}
+            {isDropdownVisible && (
+              <div className="dropdown-content" onMouseLeave={hideDropdown}>
+                <Link to="/products">All Products</Link>
+                <Link to="/products/3d-signs">3D Signs</Link>
+                <Link to="/products/2d-signs">2D Signs</Link>
+                <Link to="/products/road-signs">Road Signs</Link>
+                <Link to="/products/cnc-cuts">CNC Cuts</Link>
+                <Link to="/products/laser-cuts">Laser Cuts</Link>
+                <Link to="/products/branding">Branding</Link>
+                <Link to="/products/signages">Signages</Link>
+                <Link to="/products/paylons">Paylons</Link>
+                <Link to="/products/engravings">Engravings</Link>
+              </div>
+            )}
+          </span>
+        </div>
+        <div className="menu-item">
+          <Link to="/about-us">About Us</Link> {/* Link to the About Us page */}
+        </div>
+        <div className="menu-item">
+          <Link to="/contacts">Talk to Us</Link> {/* Link to the Contacts page */}
+        </div>
+        <div className="menu-item">
+          <Link to="/cart">
+            <FaShoppingCart /> {/* Cart Icon */}
+          </Link>
+        </div>
       </div>
 
       {/* User-related links */}
@@ -40,5 +149,3 @@ const Navbar = ({ loggedIn, logOut, cartItems }) => {
 };
 
 export default Navbar;
-
-
--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -1,68 +1,27 @@
-// import React from 'react';
-// import Card from './Card'; // Import the Card component to display products
-// // You can create a CSS file for styling
-
-// const ProductPage = ({ products }) => {
-//   return (
-//     <div className="product-page">
-//       <h1>Products</h1>
-//       <div className="product-list">
-//         {products.map((product, index) => (
-//           <Card key={index} product={product} />
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ProductPage;
-
-
-
 import React from 'react';
 import Card from './Card';
 
 const ProductPage = ({ products }) => {
-  // Sample product data (replace with your actual product data)
-  const sampleProducts = [
-    {
-      name: 'Product 1',
-      price: 19.99,
-      image: 'https://scontent.fnbo1-1.fna.fbcdn.net/v/t39.30808-6/369808310_621075770146868_2002638175875292684_n.jpg?stp=dst-jpg_p180x540&_nc_cat=104&ccb=1-7&_nc_sid=49d041&_nc_ohc=MRBTxNxXa1QAX_tZKJ1&_nc_ht=scontent.fnbo1-1.fna&oh=00_AfBJqBggOe1B9rDDTHJYCup0EwK4D5FOoxTqijqts_VLyg&oe=6501DBD2',
-    },
-    {
-      name: 'Product 2',
-      price: 24.99,
-      image: 'https://scontent.fnbo1-1.fna.fbcdn.net/v/t39.30808-6/376557903_624687363119042_499420217234291001_n.jpg?stp=dst-jpg_s600x600&_nc_cat=108&ccb=1-7&_nc_sid=49d041&_nc_ohc=jcYqe3fBVnMAX8KOAfr&_nc_ht=scontent.fnbo1-1.fna&oh=00_AfCKC91I1BT6g0Mps9Cf2JNb0LyX2gqkHeXCXKNc8gMRsA&oe=65015041',
-    },
-    {
-      name: 'Product 3',
-      price: 29.99,
-      image: 'https://scontent.fnbo1-1.fna.fbcdn.net/v/t39.30808-6/376629047_624688093118969_5428831927067817393_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=49d041&_nc_ohc=FZHj9cTdpkMAX_fRilz&_nc_ht=scontent.fnbo1-1.fna&oh=00_AfBrZ1Dv3cAsHkV-jKc9enFv66EpBW5zRkDxCN1d9OlD6A&oe=65027A1B',
-    },
-    {
-      name: 'Product 4',
-      price: 14.99,
-      image: 'https://scontent.fnbo1-1.fna.fbcdn.net/v/t39.30808-6/376537198_624688049785640_1276239699203536657_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=49d041&_nc_ohc=tKNOig1KcjcAX_l_Tng&_nc_ht=scontent.fnbo1-1.fna&oh=00_AfBh1yCk0iMS-TkTLMwuL9bV3JqjgoV2i4sNwDCCxgH3XA&oe=6500D670',
-    },
-    {
-      name: 'Product 5',
-      price: 39.99,
-      image: 'https://scontent.fnbo1-1.fna.fbcdn.net/v/t39.30808-6/376581125_624687956452316_159646435912976525_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=49d041&_nc_ohc=MSFItV_leiEAX_zACHB&_nc_ht=scontent.fnbo1-1.fna&oh=00_AfDL-ivDeqwk0AM10GmGdWceFQkIsty3EB-D3eGM8Bx9jQ&oe=65027629',
-    },
-  ];
+  // Get unique product categories
+  const categories = [...new Set(products.map((product) => product.category))];
 
   return (
     <div className="product-page">
       <h1>Products</h1>
-      <div className="product-list">
-        {sampleProducts.map((product, index) => (
-          <Card key={index} product={product} />
-        ))}
-      </div>
+      {categories.map((category) => (
+        <div key={category}>
+          <h2>{category}</h2>
+          <div className="product-list">
+            {products
+              .filter((product) => product.category === category)
+              .map((product, index) => (
+                <Card key={index} product={product} />
+              ))}
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
 
 export default ProductPage;
-
